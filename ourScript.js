3ce252652642a5cebeadf7e667ca5221766087ea
Nathaniel Rubin <narubin@umich.edu>
2014-02-22 16:19:34 -0500

    Added local bootstrap files

--- ourScript.js
+++ ourScript.js
@@ -1,3 +1,9 @@
-function loadTransportData() {
-  console.log(3);
-}
+$(document).ready(function() {
+  function loadTransportData() {
+    console.log(3);
+  }
+
+  $(".outer_row").click( function() {
+    //$("div.outer_row.focus").css("background-color", "transparent");
+  });
+});
